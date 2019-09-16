8b9848031f77c3e6997791ae3b73b740e3cef34a
andrikburik <43092668+andrikburik@users.noreply.github.com>
2019-09-16 22:57:22 +0300

    Add files via upload

---
+++ Lesson 4/Lesson 4 part 6.js
@@ -0,0 +1,16 @@
+function show(argument) {
+	console.log(argument)
+}
+
+////////// 6 ///////////
+show('-----6-----');
+
+var number = 90; 
+
+while(number >= 0) {
+	show(number);
+	number -= 5;
+}
+
+
+// 6. Створіть скрипт .js, що виводить на екран всі невід'ємні елементи послідовності 90 85 80 75 70 65 60 .... 
