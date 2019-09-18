7d5122ad9e66a9ab3fed2730a68ad871cea308d3
andrikburik <43092668+andrikburik@users.noreply.github.com>
2019-09-18 22:59:13 +0300

    Add files via upload

---
+++ Lesson 5/Lesson 5 part 1.js
@@ -0,0 +1,20 @@
+var user = {};
+
+user.name = "ПИЛИП";
+console.log(user);
+user.surname = "ШЕВЧЕНКО";
+console.log(user);
+user.name = "СЕРГІЙ";
+console.log(user);
+
+delete user.name;
+
+console.log(user);
+
+// 1). Створіть порожній об'єкт user. 
+
+// Додайте властивість name зі значенням ПИЛИП. 
+// Додайте властивість surname зі значенням ШЕВЧЕНКО. 
+// Поміняйте значення name на СЕРГІЙ. 
+// Видаліть властивість name з об'єкта. 
+
