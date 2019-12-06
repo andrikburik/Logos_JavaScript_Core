ee6e8214c0ec5808fca3d4e4cab61400ef3dc599
andrikburik <43092668+andrikburik@users.noreply.github.com>
2019-12-06 13:17:12 +0200

    Update lesson 6 part 1(2).js

--- lesson 6 part 1(2).js
+++ lesson 6 part 1(2).js
@@ -2,17 +2,9 @@ function show(argument) {
 	console.log(argument);
 };
 
-function min(numbers) {
-	var minNumber = numbers[0];
-	for (var i = 0; i < numbers.length; i++) {
-		if (minNumber > numbers[i+1]) {
-			minNumber = numbers[i+1];
-		}
-	};
-	return minNumber;
+function checkAge(age) {
+	age >= 18 ? show(true) : show('Do you have permision?');
 }
 
-var numbers = [10, 2, -18, 27, 237, 0]
-
-show(min(numbers));
+checkAge(12);
 
